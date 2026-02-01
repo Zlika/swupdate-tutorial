2160381f7f994c0860693c18def8308a2a6b9f2c
Thomas Lorblanchès <zlika_ese@hotmail.com>
2026-02-01 14:14:31 +0100

    Ajout de buildroot en submodule + arrêt des scripts à la première erreur + renommage du dossier overlay rootfs

--- double-copy/board/copy-swupdate-pubkey.sh
+++ double-copy/board/copy-swupdate-pubkey.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 # Génération de la clef publique RSA pour swupdate à partir de la clef privée
 openssl rsa -in "$BR2_EXTERNAL_DOUBLE_COPY_PATH/board/swupdate-priv.pem" -out "$TARGET_DIR/etc/swupdate/swupdate-public.pem" -outform PEM -pubout
 
--- double-copy/board/gen-swupdate-image.sh
+++ double-copy/board/gen-swupdate-image.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+set -e
+
 # Copie des fichiers à mettre dans l'archive swupdate dans le répertoire de sortie
 cp $BR2_EXTERNAL_DOUBLE_COPY_PATH/board/sw-description $BR2_EXTERNAL_DOUBLE_COPY_PATH/output/images/
 cp $BR2_EXTERNAL_DOUBLE_COPY_PATH/board/update_boot.sh $BR2_EXTERNAL_DOUBLE_COPY_PATH/output/images/
+
 # Calcul du hash de l'image
 IMAGE_HASH=$(sha256sum $BR2_EXTERNAL_DOUBLE_COPY_PATH/output/images/rootfs.ext2.gz | cut -d ' ' -f 1)
 sed -i "s/.*IMAGE_HASH.*/\t\t\tsha256 = \"$IMAGE_HASH\";/" $BR2_EXTERNAL_DOUBLE_COPY_PATH/output/images/sw-description
+
 # Signature du fichier sw-description avec la clef secrète
 openssl dgst -sha256 -sign $BR2_EXTERNAL_DOUBLE_COPY_PATH/board/swupdate-priv.pem $BR2_EXTERNAL_DOUBLE_COPY_PATH/output/images/sw-description > $BR2_EXTERNAL_DOUBLE_COPY_PATH/output/images/sw-description.sig
+
 # Génération de l'archive swupdate
 cd $BR2_EXTERNAL_DOUBLE_COPY_PATH/output/images
 SWU_FILES="sw-description sw-description.sig update_boot.sh rootfs.ext2.gz"
