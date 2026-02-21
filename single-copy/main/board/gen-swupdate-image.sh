691e3ee22ac74318bb97f8e54072bfb45ff1263a
Thomas Lorblanchès <zlika_ese@hotmail.com>
2026-02-21 16:01:23 +0100

    Corrections single-copy

--- single-copy/main/board/gen-swupdate-image.sh
+++ single-copy/main/board/gen-swupdate-image.sh
@@ -7,9 +7,9 @@ cp $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/board/update_boot.sh $BR2_EXTERNAL_SINGLE
 
 # Calcul des hashs des images
 RECOVERY_IMAGE_HASH=$(sha256sum $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/output/images/recovery.vfat | cut -d ' ' -f 1)
-sed -i "s/.*RECOVERY_IMAGE_HASH.*/\t\t\tsha256sum = \"$RECOVERY_IMAGE_HASH\";/" $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/output/images/sw-description
+sed -i "s/.*RECOVERY_IMAGE_HASH.*/\t\t\tsha256 = \"$RECOVERY_IMAGE_HASH\";/" $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/output/images/sw-description
 MAIN_IMAGE_HASH=$(sha256sum $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/output/images/rootfs.ext2.gz | cut -d ' ' -f 1)
-sed -i "s/.*MAIN_IMAGE_HASH.*/\t\t\tsha256sum = \"$MAIN_IMAGE_HASH\";/" $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/output/images/sw-description
+sed -i "s/.*MAIN_IMAGE_HASH.*/\t\t\tsha256 = \"$MAIN_IMAGE_HASH\";/" $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/output/images/sw-description
 
 # Signature du fichier sw-description avec la clef secrète
 openssl dgst -sha256 -sign $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/board/swupdate-priv.pem $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/output/images/sw-description > $BR2_EXTERNAL_SINGLE_COPY_MAIN_PATH/output/images/sw-description.sig
